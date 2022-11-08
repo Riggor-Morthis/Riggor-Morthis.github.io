2d8e3f3964de282af8fe28b1a411f6bda552f62b
Riggor Morthis <roc.syl@outlook.com>
2022-11-08 22:18:04 +0100

    more content

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var personnalDiv;
 var educationDiv;
+var experienceDiv;
 
 var personnalButton;
 var educationButton;
+var experienceButton;
 
 window.onload = function(){
     personnalDiv = document.getElementById("personnalDiv");
     educationDiv = document.getElementById("educationDiv");
+    experienceDiv = document.getElementById("experienceDiv");
 
     personnalButton = document.getElementById("personnalButton");
     educationButton = document.getElementById("educationButton");
+    experienceButton = documen.getElementById("experienceButton");
 
     ShowPersonnalProjects();
 }
@@ -17,15 +21,29 @@ window.onload = function(){
 function ShowPersonnalProjects(){
     personnalDiv.style.display = "flex";
     educationDiv.style.display = "none";
+    experienceDiv.style.display = "none";
 
     personnalButton.className = "divButtonClicked";
     educationButton.className = "divButton";
+    experienceButton.className = "divButton";
 }
 
 function ShowEducationProjects(){
     personnalDiv.style.display = "none";
     educationDiv.style.display = "flex";
+    experienceDiv.style.display = "none";
 
     personnalButton.className = "divButton";
     educationButton.className = "divButtonClicked";
+    experienceButton.className = "divButton";
+}
+
+function ShowProfessionalExperience(){
+    personnalDiv.style.display = "none";
+    educationDiv.style.display = "none";
+    experienceDiv.style.display = "flex";
+
+    personnalButton.className = "divButton";
+    educationButton.className = "divButton";
+    experienceButton.className = "divButtonClicked";
 }
