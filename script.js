2176ba58be0d57f828d24d548da46c605e6399f0
Riggor Morthis <roc.syl@outlook.com>
2022-11-08 17:51:11 +0100

    better buttons

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var personnalDiv;
 var educationDiv;
 
+var personnalButton;
+var educationButton;
+
 window.onload = function(){
     personnalDiv = document.getElementById("personnalDiv");
     educationDiv = document.getElementById("educationDiv");
+
+    personnalButton = document.getElementById("personnalButton");
+    educationButton = document.getElementById("educationButton");
 
     ShowPersonnalProjects();
 }
@@ -11,9 +17,15 @@ window.onload = function(){
 function ShowPersonnalProjects(){
     personnalDiv.style.display = "flex";
     educationDiv.style.display = "none";
+
+    personnalButton.className = "divButtonClicked";
+    educationButton.className = "divButton";
 }
 
 function ShowEducationProjects(){
     personnalDiv.style.display = "none";
     educationDiv.style.display = "flex";
+
+    personnalButton.className = "divButton";
+    educationButton.className = "divButtonClicked";
 }
