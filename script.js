dc43b2004c2d6f1e7f782f9b63c5f5033e06e3fb
Riggor Morthis <roc.syl@outlook.com>
2022-11-08 16:43:48 +0100

    working buttons

---
+++ script.js
@@ -0,0 +1,19 @@
+var personnalDiv;
+var educationDiv;
+
+document.addEventListener("load", FindDivs);
+
+function FindDivs(){
+    personnalDiv = document.getElementById("personnalDiv");
+    educationDiv = document.getElementById("educationDiv");
+}
+
+function ShowPersonnalProjects(){
+    personnalDiv.style = "display:flex";
+    educationDiv.style = "display:none";
+}
+
+function ShowEducationProjects(){
+    personnalDiv.style = "display:none";
+    educationDiv.style = "display:flex";
+}
