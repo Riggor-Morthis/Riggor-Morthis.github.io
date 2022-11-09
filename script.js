5c51158649c71b6600117a1ed069f9f7e0df88c0
Riggor Morthis <roc.syl@outlook.com>
2022-11-09 10:41:52 +0100

    more content

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 var personnalDiv;
 var educationDiv;
 var experienceDiv;
+var identityDiv;
 
 var personnalButton;
 var educationButton;
 var experienceButton;
+var identityButton;
 
 window.onload = function(){
     personnalDiv = document.getElementById("personnalDiv");
     educationDiv = document.getElementById("educationDiv");
     experienceDiv = document.getElementById("experienceDiv");
+    identityDiv = document.getElementById("identityDiv");
 
     personnalButton = document.getElementById("personnalButton");
     educationButton = document.getElementById("educationButton");
     experienceButton = document.getElementById("experienceButton");
+    identityButton = document.getElementById("identityButton");
 
     ShowPersonnalProjects();
 }
@@ -22,28 +26,46 @@ function ShowPersonnalProjects(){
     personnalDiv.style.display = "flex";
     educationDiv.style.display = "none";
     experienceDiv.style.display = "none";
+    identityDiv.style.display = "none";
 
     personnalButton.className = "divButtonClicked";
     educationButton.className = "divButton";
     experienceButton.className = "divButton";
+    identityButton.className = "divButton";
 }
 
 function ShowEducationProjects(){
     personnalDiv.style.display = "none";
     educationDiv.style.display = "flex";
     experienceDiv.style.display = "none";
+    identityDiv.style.display = "none";
 
     personnalButton.className = "divButton";
     educationButton.className = "divButtonClicked";
     experienceButton.className = "divButton";
+    identityButton.className = "divButton";
 }
 
 function ShowProfessionalExperience(){
     personnalDiv.style.display = "none";
     educationDiv.style.display = "none";
     experienceDiv.style.display = "flex";
+    identityDiv.style.display = "none";
 
     personnalButton.className = "divButton";
     educationButton.className = "divButton";
     experienceButton.className = "divButtonClicked";
+    identityButton.className = "divButton";
+}
+
+function ShowIdentity(){
+    personnalDiv.style.display = "none";
+    educationDiv.style.display = "none";
+    experienceDiv.style.display = "none";
+    identityDiv.style.display = "flex";
+
+    personnalButton.className = "divButton";
+    educationButton.className = "divButton";
+    experienceButton.className = "divButton";
+    identityButton.className = "divButtonClicked";
 }
